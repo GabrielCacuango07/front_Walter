77f1cd7fe671351630fafedfcb906f4c7334cca7
GabrielCacuango07 <gabrielcacuango07@gmail.com>
2021-07-16 21:53:43 -0500

    proyecto con imngreso por gmail

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {CartModelServer} from '../../models/cart.model';
+import {CartService} from '../../services/cart.service';
+import {UserService} from '../../services/user.service';
+import { TranslateService } from '@ngx-translate/core';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  cartData: CartModelServer;
+  cartTotal: number;
+  public activeLang = 'es';
+  authState: boolean;
+  constructor(public cartService: CartService,
+    private translate: TranslateService,
+              public userService: UserService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+
+    this.cartService.cartData$.subscribe(data => this.cartData = data);
+
+    this.userService.authState$.subscribe(authState => this.authState = authState);
+  }
+
+  public cambiarLenguaje(lang) {
+    this.activeLang = lang;
+    this.translate.use(lang);
+  }
+
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {Router} from '@angular/router';
+import {ProductModelServer, ServerResponse} from '../../models/product.model';
+import {CartService} from '../../services/cart.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  products: ProductModelServer[] = [];
+
+
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private router: Router) { }
+
+              showMainContent: Boolean = true;
+              ngOnInit(): void {
+
+    this.productService.getAllProducts().subscribe((prods: ServerResponse) => {
+      this.products = prods.products;
+    });
+    console.log(this.productService)
+  }
+
+  selectProduct(id: number) {
+    this.router.navigate(['/product', id]).then();
+  }
+
+  AddToCart(id: number) {
+    this.cartService.AddProductToCart(id);
+  }
+  AddProduct(id: number) {
+    this.cartService.AddProductToCart(id);
+  }
+
+  ShowHideButton() {
+    this.showMainContent = this.showMainContent ? false : true;
+ }
+}
